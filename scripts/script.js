c94aaf3475755cc7c2dcf3fd01cdc2b4ae36fe6e
pranoy <gpranoy@gmail.com>
2018-02-15 15:16:07 +0530

    al elements are in place
    with scroll animation control through JS also set up.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
-function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
+document.onload = function(){
+	// var bus = document.createAttribute("data-0");
+	// bus.value = "left: -120%; filter: blur(2px)";
+	// document.getElementById("bus").setAttributeNode(bus);
+
+	document.getElementById("bus").setAttribute("data-0","left: -120%; filter: blur(2px)");
+	var s = skrollr.init(); // Used to initialise Scrollr library.
 }
+// data-0="left: -120%; filter: blur(2px)" 
+//         data-500="left:30%; filter: blur(0px)" 
+//         data-800="left:30%;" 
+//         data-900="left: 100%;"
